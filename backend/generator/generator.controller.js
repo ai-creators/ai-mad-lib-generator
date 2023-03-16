2b2a7d1195911b6fb943e4e70d456a6c7e2d60ab
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-03-16 03:11:58 -0400

    test

--- backend/generator/generator.controller.js
+++ backend/generator/generator.controller.js
@@ -1,13 +1,58 @@
-//controller file for interacting with openapi generator
+//openai api key =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
 
-import { Request, Response } from 'express';
-import { OpenAPIGenerator } from './generator.service';
+const openai = require('openai');
+const express = require('express');
+const router = express.Router();
 
-export class GeneratorController {
-    public async generate(req: Request, res: Response) {
-        const generator = new OpenAPIGenerator();
-        const result = await generator.generate(req.body);
-        res.send(result);
-    }
-    }
+// Set up OpenAI API credentials
+openai.apiKey = 'YOUR_API_KEY';
 
+// Define the Mad Lib template
+const template = 'One [adjective] day, I [verb] to the [noun] to buy some [plural noun].';
+
+// Use OpenAI API to generate suggestions for words
+async function generateSuggestions(prompt) {
+  const completions = await openai.complete({
+    engine: 'davinci',
+    prompt: prompt,
+    maxTokens: 50,
+    n: 1,
+    stop: null,
+    temperature: 0.5,
+  });
+  const message = completions.choices[0].text.trim();
+  return message;
+}
+
+// Define the endpoint for the Mad Lib generator
+router.post('/madlib', async (req, res) => {
+  try {
+    // Generate suggestions for words
+    const adjective = await generateSuggestions('Suggest an adjective');
+    const verb = await generateSuggestions('Suggest a verb');
+    const noun = await generateSuggestions('Suggest a noun');
+    const pluralNoun = await generateSuggestions('Suggest a plural noun');
+
+    // Get user input for words
+    const userInput = {
+      adjective: req.body.adjective,
+      verb: req.body.verb,
+      noun: req.body.noun,
+      pluralNoun: req.body.pluralNoun,
+    };
+
+    // Generate the completed Mad Lib
+    let madLib = template;
+    madLib = madLib.replace('[adjective]', userInput.adjective || adjective);
+    madLib = madLib.replace('[verb]', userInput.verb || verb);
+    madLib = madLib.replace('[noun]', userInput.noun || noun);
+    madLib = madLib.replace('[plural noun]', userInput.pluralNoun || pluralNoun);
+
+    res.json({ madLib: madLib });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+module.exports = router;
