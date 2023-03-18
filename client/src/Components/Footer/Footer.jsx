55a4267a362d3e837906cd5834d1fc42315d07ac
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-03-18 15:11:55 -0400

    HACK-8 Setup and update Footer styling.

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="text-center py-4">Website made with 💙</footer>;
+  return (
+    <footer className="text-center py-4 bg-slate-900 text-white">
+      Website made with 💙
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/Components/MadLibBuilder/MadLibBuilder.jsx
+++ client/src/Components/MadLibBuilder/MadLibBuilder.jsx
@@ -1,7 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const MadLibBuilder = ({ madlib }) => {
-  const [answers, setAnswers] = useState();
+  const [answers, setAnswers] = useState([]);
+  const [formattedMadLib, setFormattedMadLib] = useState("");
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    if (madlib) {
+      let questionType = "";
+      let isInBracket = false;
+      for (const letter of madlib) {
+        if (letter === "[") {
+          isInBracket = true;
+        }
+        if (letter === "]") {
+          isInBracket = false;
+          const question = {
+            question: questionType,
+            answer: "",
+          };
+        }
+        if (isInBracket) {
+          questionType += letter;
+        }
+      }
+    }
+  }, [madlib]);
+
   return <form></form>;
 };
 
