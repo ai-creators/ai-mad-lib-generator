da1ca926de1b8631cee9ff01dab514d9521cff19
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-03-18 21:24:55 -0400

    Add error handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 const express = require("express");
-
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
 const app = express();
+
+app.use(express.json);
 
 // routes
 
-app.use(express.json);
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
---
+++ backend/src/errors/methodNotAllowed.js
@@ -0,0 +1,12 @@
+/**
+ * Express Router "Method not allowed" handler.
+ * add to end of router for the rest of the different type of request * * to be proccessed
+ */
+function methodNotAllowed(req, res, next) {
+  next({
+    status: 405,
+    message: `${req.method} not allowed for ${req.originalUrl}`,
+  });
+}
+
+module.exports = methodNotAllowed;
