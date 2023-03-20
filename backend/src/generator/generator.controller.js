8317baaeff3ad14d9d7fe023561805b76dc00914
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-03-19 21:36:49 -0400

    Co-authored-by: Anthony McLamb <admclamb@users.noreply.github.com>

--- backend/src/generator/generator.controller.js
+++ backend/src/generator/generator.controller.js
@@ -1,6 +1,52 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const { Configuration, OpenAIApi } = require("openai");
 
+async function configureOpenAi(req,res,next) {
+  try {
+    console.log("CONFIGURING");
+    
+    res.locals.openai = openai
+    return next();
+  } catch(error) {
+    return next({ status: 500, message: error.message});
+  }
+}
+
+async function getMadLib(req,res,next) {
+  try {
+    const { OPENAI_API_KEY }= process.env;
+    if (!OPENAI_API_KEY) {
+      throw new Error("No open ai key has been provided");
+    }
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    console.log("GET MAD LIB");
+    const { prompt } = req.body.data;
+    console.log("prompt: ", prompt)
+    const { data } = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `Generate mad lib with using [adjective: ] and no spaces inside the bracket. The mad lib will be: ${prompt}`,
+      max_tokens: 100,
+      temperature: 0
+    })
+    console.log("RESPONSE: ", data.choices[0].text);
+    res.status(200).json({ data });
+  } catch(error) {
+    console.error(error);
+    return next({
+      status: 500, message: error
+    })
+  }
+}
+
+// Export the controller
+module.exports = {
+  madLibGenerator: [ asyncErrorBoundary(getMadLib)],
+};
+
+
 // const response = await openai.createCompletion({
 //   model: "text-davinci-003",
 //   prompt: "Say this is a test",
@@ -27,81 +73,36 @@ const { Configuration, OpenAIApi } = require("openai");
 //   return message;
 // }
 
-// // Define the endpoint for the Mad Lib generator
-// async function madLibGenerator(req, res) {
-//   try {
-//     // Generate suggestions for words
-//     const adjective = await generateSuggestions("Suggest an adjective");
-//     const verb = await generateSuggestions("Suggest a verb");
-//     const noun = await generateSuggestions("Suggest a noun");
-//     const pluralNoun = await generateSuggestions("Suggest a plural noun");
-
-//     // Get user input for words
-//     const userInput = {
-//       adjective: req.body.adjective,
-//       verb: req.body.verb,
-//       noun: req.body.noun,
-//       pluralNoun: req.body.pluralNoun,
-//     };
-
-//     // Generate the completed Mad Lib
-//     let madLib = template;
-//     madLib = madLib.replace("[adjective]", userInput.adjective || adjective);
-//     madLib = madLib.replace("[verb]", userInput.verb || verb);
-//     madLib = madLib.replace("[noun]", userInput.noun || noun);
-//     madLib = madLib.replace(
-//       "[plural noun]",
-//       userInput.pluralNoun || pluralNoun
-//     );
-
-//     res.json({ madLib: madLib });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "An error occurred" });
-//   }
-// }
-
-async function configureOpenAi(req,res,next) {
+// Define the endpoint for the Mad Lib generator
+async function madLibGenerator(req, res) {
   try {
-    console.log("CONFIGURING");
-    
-    res.locals.openai = openai
-    return next();
-  } catch(error) {
-    return next({ status: 500, message: error.message});
-  }
-}
+    // Generate suggestions for words
+    const adjective = await generateSuggestions("Suggest an adjective");
+    const verb = await generateSuggestions("Suggest a verb");
+    const noun = await generateSuggestions("Suggest a noun");
+    const pluralNoun = await generateSuggestions("Suggest a plural noun");
 
-async function getMadLib(req,res,next) {
-  try {
-    const { OPENAI_API_KEY }= process.env;
-    if (!OPENAI_API_KEY) {
-      throw new Error("No open ai key has been provided");
-    }
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-    console.log("GET MAD LIB");
-    const { prompt } = req.body.data;
-    console.log("prompt: ", prompt);
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt,
-      max_tokens: 7,
-      temperature: 0
-    })
-    console.log("REPSONSE: ", response);
-    res.status(200).json({ data: response});
-  } catch(error) {
+    // Get user input for words
+    const userInput = {
+      adjective: req.body.adjective,
+      verb: req.body.verb,
+      noun: req.body.noun,
+      pluralNoun: req.body.pluralNoun,
+    };
+
+    // Generate the completed Mad Lib
+    let madLib = template;
+    madLib = madLib.replace("[adjective]", userInput.adjective || adjective);
+    madLib = madLib.replace("[verb]", userInput.verb || verb);
+    madLib = madLib.replace("[noun]", userInput.noun || noun);
+    madLib = madLib.replace(
+      "[plural noun]",
+      userInput.pluralNoun || pluralNoun
+    );
+
+    res.json({ madLib: madLib });
+  } catch (error) {
     console.error(error);
-    return next({
-      status: 500, message: error
-    })
+    res.status(500).json({ error: "An error occurred" });
   }
 }
-
-// Export the controller
-module.exports = {
-  madLibGenerator: [ asyncErrorBoundary(getMadLib)],
-};
