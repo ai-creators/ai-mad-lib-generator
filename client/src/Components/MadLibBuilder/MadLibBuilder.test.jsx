41df7586df42da85886033b809275cbad9082e33
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-03-30 21:57:57 -0400

    Update test

--- client/src/Components/MadLibBuilder/MadLibBuilder.test.jsx
+++ client/src/Components/MadLibBuilder/MadLibBuilder.test.jsx
@@ -19,17 +19,34 @@ describe("MadLibBuilder", () => {
     expect(noMadLibText).toEqual("No MadLib provided");
   });
 
-  it("Should return proper text when MadLib is not properly formatted with brackets", () => {
-    const invalidMadLib = "This is a test and does not include brackets";
-    render(<MadLibBuilder madLib={invalidMadLib} />);
-    const noMadLibText = screen.getByTestId("no-madlib-text").textContent;
-    expect(noMadLibText).toEqual("MadLib incorrectly formatted");
-  });
-
   it("Should return a form to build the madlib when it is passed the correct madlib", () => {
     const correctMadLib = "This is a [adjective] correct madlib";
     render(<MadLibBuilder madLib={correctMadLib} />);
     const madLibForm = screen.getByTestId("madlib-builder-form");
     expect(madLibForm).toBeDefined();
+  });
+
+  it("Should return proper text when MadLib is not properly formatted with brackets", () => {
+    const invalidMadLib =
+      "This is a test with [brackets improperly [formatted]";
+    render(<MadLibBuilder madLib={invalidMadLib} />);
+    const noMadLibText = screen.getByTestId("no-madlib-text").textContent;
+    expect(noMadLibText).toEqual("MadLib incorrectly formatted");
+  });
+
+  it("Should return proper text when MadLib is not properly formatted with brackets", () => {
+    const invalidMadLib =
+      "This is a test with brackets] improperly [formatted]";
+    render(<MadLibBuilder madLib={invalidMadLib} />);
+    const noMadLibText = screen.getByTestId("no-madlib-text").textContent;
+    expect(noMadLibText).toEqual("MadLib incorrectly formatted");
+  });
+
+  it("Should return proper text when MadLib is not properly formatted with brackets", () => {
+    const invalidMadLib =
+      "This is a test with brackets] improperly [formatted]";
+    render(<MadLibBuilder madLib={invalidMadLib} />);
+    const noMadLibText = screen.getByTestId("no-madlib-text").textContent;
+    expect(noMadLibText).toEqual("MadLib incorrectly formatted");
   });
 });
