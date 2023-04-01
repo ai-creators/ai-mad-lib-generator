021edd0a61d5160e122bc778695ef96741e5dd52
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-03-31 23:12:01 -0400

    test

--- backend/src/generator/generator.controller.js
+++ backend/src/generator/generator.controller.js
@@ -45,65 +45,9 @@ async function getMadLib(req, res, next) {
   }
 }
 
-// const response = await openai.createCompletion({
-//   model: "text-davinci-003",
-//   prompt: "Say this is a test",
-//   max_tokens: 7,
-//   temperature: 0,
-// });
-//openai api key =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
-
-// Define the Mad Lib template
-// const template =
-//   "One [adjective] day, I [verb] to the [noun] to buy some [plural noun].";
-
-// // Use OpenAI API to generate suggestions for words
-// async function generateSuggestions(prompt) {
-//   const completions = await openai.createCompletion({
-//     engine: "davinci",
-//     prompt: prompt,
-//     maxTokens: 50,
-//     n: 1,
-//     stop: null,
-//     temperature: 0.5,
-//   });
-//   const message = completions.choices[0].text.trim();
-//   return message;
-// }
-
-// Define the endpoint for the Mad Lib generator
-// async function madLibGenerator(req, res) {
-//   try {
-//     // Generate suggestions for words
-//     const adjective = await generateSuggestions("Suggest an adjective");
-//     const verb = await generateSuggestions("Suggest a verb");
-//     const noun = await generateSuggestions("Suggest a noun");
-//     const pluralNoun = await generateSuggestions("Suggest a plural noun");
-
-//     // Get user input for words
-//     const userInput = {
-//       adjective: req.body.adjective,
-//       verb: req.body.verb,
-//       noun: req.body.noun,
-//       pluralNoun: req.body.pluralNoun,
-//     };
-
-//     // Generate the completed Mad Lib
-//     let madLib = template;
-//     madLib = madLib.replace("[adjective]", userInput.adjective || adjective);
-//     madLib = madLib.replace("[verb]", userInput.verb || verb);
-//     madLib = madLib.replace("[noun]", userInput.noun || noun);
-//     madLib = madLib.replace(
-//       "[plural noun]",
-//       userInput.pluralNoun || pluralNoun
-//     );
-
-//     res.json({ madLib: madLib });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "An error occurred" });
-//   }
-// }
+module.exports = {
+  getMadLib: [asyncErrorBoundary(getMadLib)],
+};
 
 // async function getMadLib(req, res, next) {
 //   try {
--- backend/test/test.js
+++ backend/test/test.js
@@ -2,28 +2,62 @@ const chai = require("chai");
 const expect = chai.expect;
 const app = require("../src/app");
 const request = require("supertest");
-const assert = require("assert");
 
 describe("MadLib Generator API", function () {
-  describe("POST /generate/madlib", function () {
-    // Test case to check if the MadLib generator can return a completed MadLib for valid input
-    it("should return a completed MadLib for valid input", function (done) {
-      request(app)
-        .post("/generate/madlib")
-        .send({
-          template: "I went to the store and bought a [noun].",
-          words: ["apple"],
-        })
-        .expect(200)
-        .end(function (err, res) {
-          if (err) return done(err);
-          expect(res.body).to.have.property("madlib");
-          expect(res.body.madlib).to.equal("I went to the store and bought a apple.");
-          done();
-        });
-    });
-    
+  it("returns a 200 response with a valid madlib", function (done) {
+    request(app)
+      .post("/madlib")
+      .send({ template: "I am {adjective} to be learning {language}." })
+      .send({ words: { adjective: "excited", language: "JavaScript" } })
+      .set("x-api-key", "test")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body).to.have.property("madlib");
+        expect(res.body.madlib).to.equal("I am excited to be learning JavaScript.");
+        done();
+      });
+  });
 
+  it("returns a 400 response with an error message if template is missing", function (done) {
+    request(app)
+      .post("/madlib")
+      .send({ words: { adjective: "excited", language: "JavaScript" } })
+      .set("x-api-key", "test")
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body).to.have.property("error");
+        expect(res.body.error).to.equal("Missing template.");
+        done();
+      });
+  });
+
+  it("returns a 400 response with an error message if words are missing", function (done) {
+    request(app)
+      .post("/madlib")
+      .send({ template: "I am {adjective} to be learning {language}." })
+      .set("x-api-key", "test")
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body).to.have.property("error");
+        expect(res.body.error).to.equal("Missing words.");
+        done();
+      });
+  });
+
+  it("returns a 500 response with an error message if OpenAI API key is missing", function (done) {
+    request(app)
+      .post("/madlib")
+      .send({ template: "I am {adjective} to be learning {language}." })
+      .send({ words: { adjective: "excited", language: "JavaScript" } })
+      .expect(500)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body).to.have.property("error");
+        expect(res.body.error).to.equal("OpenAI API key missing.");
+        done();
+      });
   });
 });
-
