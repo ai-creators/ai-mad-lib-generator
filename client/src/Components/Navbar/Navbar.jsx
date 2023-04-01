42ce1f527a8fc4583b797785d3677813d85d1e10
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-03-31 20:36:46 -0400

    Fix Nav issue

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="py-4 border-b">
       <div className="container mx-auto">
-        <h1 className="font-semibold text-xl">Mad Lib Generator</h1>
+        <Link to="/">
+          <h1 className="font-semibold text-xl">Mad Lib Generator</h1>
+        </Link>
       </div>
     </nav>
   );
