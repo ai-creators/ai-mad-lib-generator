e7c13428ff98b2a84e8bf045ec0dc82d9e8d33c5
admclamb <adylanmclamb@yahoo.com>
2023-04-04 17:35:11 -0400

    Init controller

--- backend/src/lib/lib.controller.js
+++ backend/src/lib/lib.controller.js
@@ -1,5 +1,11 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const service = require("./lib.service");
+
+async function listLibs(req, res, next) {
+  res.status(200).json({ data: await service.list() });
+}
 
 module.exports = {
   list: [],
+  save: [],
 };
