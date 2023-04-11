8b7196b17dcde2df457a40eee5453b88ad5c9537
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-04-11 00:40:21 -0400

    asdf

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,7 +31,7 @@ app.use("/libs", libsRoute);
 app.use(limiter);
 app.use("/generate", generatorRoute);
 
-app.set("trust proxy", 5);
+app.set("trust proxy", 6);
 app.use("/api/", (req, res) => res.send(req.ip));
 app.use(notFound);
 app.use(errorHandler);
