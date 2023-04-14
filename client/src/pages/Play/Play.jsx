5a1e03a462254e9d5e461d83ac569e05b2bba3f6
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-04-14 00:14:35 -0400

    Add name input

--- client/src/pages/Play/Play.jsx
+++ client/src/pages/Play/Play.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Layout from "../../Layout/Layout";
 import ErrorAlert from "../../errors/ErrorAlert";
 import Hero from "../../Components/Hero/Hero";
+import NameInput from "../../Components/NameInput/NameInput";
 const Play = () => {
   const [error, setError] = useState(null);
   return (
@@ -11,7 +12,11 @@ const Play = () => {
         hero={<Hero title="Ai Against Humanity" />}
       >
         <ErrorAlert error={error} setError={setError} />
-        <div className="flex flex-col gap-4 pt-4"></div>
+        <div className="max-w-4xl mx-auto p-4">
+          <div className="flex flex-col gap-4 pt-4">
+            <NameInput />
+          </div>
+        </div>
       </Layout>
     </>
   );
