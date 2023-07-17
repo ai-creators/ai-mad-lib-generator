9fb623f83e4fd68696a43656de1e11528caa0ae7
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-07-16 22:47:21 -0400

    Update controller

---
+++ backend_v1/src/generator/GeneratorRouter.ts
@@ -0,0 +1,5 @@
+import express, { Router } from "express";
+
+export class GeneratorRouter {
+  private static router: Router = express.Router();
+}
--- backend_v1/src/index.ts
+++ backend_v1/src/index.ts
@@ -1,3 +1,5 @@
+import path from "path";
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 const { PORT = 5000, NODE_ENV } = process.env;
 import mongoose from "mongoose";
 import app from "./app";
