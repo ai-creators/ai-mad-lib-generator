1276ac905bc51b9b05b6d85762388b419cf5cf6f
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-07-18 23:52:11 -0400

    Update service

--- backend_v1/src/services/adlib/AdLibService.ts
+++ backend_v1/src/services/adlib/AdLibService.ts
@@ -3,6 +3,7 @@ import { Pagination } from "../../common/pagination/Pagination";
 import { IAdLib } from "../../ts/Interfaces/IAdLibs";
 import { PaginationResponse } from "../../ts/types/PaginationResponse";
 import { Service } from "../../common/Service";
+import { DatabaseConfig } from "../../db/DatabaseConfig";
 export class AdLibService implements Service {
   public getLibs(
     timestamp: Date,
@@ -10,14 +11,6 @@ export class AdLibService implements Service {
     pagination: number
   ): Promise<PaginationResponse<IAdLib>> {
     const pager = new Pagination(AdLib);
-    return pager.pageable(
-      {
-        updatedAt: {
-          $lt: timestamp,
-        },
-      },
-      page,
-      pagination
-    );
+    return pager.pageable({}, page, pagination);
   }
 }
