28b8ec093230c4ad3cb1928fe9dd1bff167a89db
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-07-20 16:46:43 -0400

    Setting up for a better pagination

---
+++ backend_v1/src/common/pagination/Pagination_v1.ts
@@ -0,0 +1,36 @@
+import { Model, Query } from "mongoose";
+
+export class Pagination<T> {
+  constructor(model: Model<T>, page: number, pagination: number) {
+    this.model = model;
+    this.total = 0;
+    this.pageAmount = 0;
+    this.page = page;
+    this.pagination = pagination;
+    this.documents = [];
+  }
+
+  public async find(conditional: {}): Promise<Query<any>> {
+    this.total = await this.calcualteTotal(conditional);
+    this.pageAmount = this.calcaultePageAmount();
+    return this.model.find(conditional);
+  }
+
+  public sort(arg: Object | String | Array<Array<string | number>): 
+
+  private async calcualteTotal(conditional: {}): Promise<number> {
+    return this.model.countDocuments(conditional);
+  }
+
+  private calcaultePageAmount(): number {
+    return Math.ceil(this.total / this.pagination);
+  }
+
+  private model: Model<T>;
+
+  private total: number;
+  private pageAmount: number;
+  private page: number;
+  private pagination: number;
+  private documents: T[];
+}
