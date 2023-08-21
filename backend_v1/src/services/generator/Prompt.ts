a03d76821f5d4b3cc6d40f21e58601acf412bdba
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-08-20 20:27:16 -0400

    test

--- backend_v1/src/services/generator/Prompt.ts
+++ backend_v1/src/services/generator/Prompt.ts
@@ -1,13 +1,14 @@
 const filterOptions = ["funny", "serious", "crazy", "neutral"];
 
 export class Prompt {
-  private static readonly PROMPT_RULES: string = 'Default rules';
+  private static readonly PROMPT_RULES: string = "Generate mad lib to fill out using [] for each replacement word to fill in. The brackets should have what the replacement is such as: adjective, noun, verb plurar noun, etc. If it's the same word suffix the word in the brackets with the number. Do not include spaces in the bracket but instead underscores.";
   private patreonLevel: "silver" | "gold" | "platinum";
   private originalPrompt: string;
   private promptLimits: string;
   private minSentences: string = '';
   private promptLength: string = '';
   private filter: "funny" | "serious" | "crazy" | "neutral" = "neutral";
+  private responseType: string = ""; // added this property
 
   constructor(originalPrompt: string, patreonLevel: "silver" | "gold" | "platinum" = "silver", filter: "funny" | "serious" | "crazy" | "neutral" = "neutral") {
     this.originalPrompt = originalPrompt;
@@ -16,8 +17,7 @@ export class Prompt {
     }
 
     this.patreonLevel = patreonLevel;
-
-    this.setFilter(filter, filterOptions);
+    this.setFilter(filter);
 
     this.promptLimits = "The mad lib cannot exceed 500 tokens."; // default limit
     this.setPromptLimits();
@@ -53,7 +53,7 @@ export class Prompt {
     return this.filter;
   }
 
-  public setFilter(filter: "funny" | "serious" | "crazy" | "neutral", filterOptions: string[]): void {
+  public setFilter(filter: "funny" | "serious" | "crazy" | "neutral"): void {
     if (filterOptions.includes(filter)) {
       this.filter = filter;
     } else {
@@ -62,26 +62,16 @@ export class Prompt {
   }
 
   public getPrompt(): string {
-<<<<<<< HEAD
     return `${Prompt.PROMPT_RULES} ${this.promptLimits} ${this.minSentences} ${this.promptLength} Filter: ${this.filter} ${this.originalPrompt}`;
   }
 
-  public getPatreonLevel(): "silver" | "gold" | "platinum" {
-    return this.patreonLevel;
-  }
-=======
-    return `${Prompt.PROMPT_RULES} ${this.promptLimits}. Make the mad lib a mininum of 3 sentences and 50 words. ${this.originalPrompt}`;
-  }
-
   public setResponseAsJSON(): void {
-    this.responseType =
-      "Respond in json format with a key of adlib and the value of the madlib response. add a key of error and value of the reason the mad lib cannot be created.";
+    this.responseType = "Respond in json format with a key of adlib and the value of the madlib response. add a key of error and value of the reason the mad lib cannot be created.";
   }
 
-  private static readonly PROMPT_RULES: string =
-    "Generate mad lib to fill out using [] for each replacement word to fill in. The brackets should have what the replacement is such as: adjective, noun, verb plurar noun, etc. If it's the same word suffix the word in the brackets with the number. Do not include spaces in the bracket but instead underscores.";
+  // Removed redundant declaration: private static readonly PROMPT_RULES: string =
+  //"Generate mad lib to fill out using [] for each replacement word to fill in. The brackets should have what the replacement is such as: adjective, noun, verb plurar noun, etc. If it's the same word suffix the word in the brackets with the number. Do not include spaces in the bracket but instead underscores.";
   private originalPrompt: string = "";
   private promptLimits: string = "";
   private responseType: string = "";
->>>>>>> f70d90f9f162c347ed7283e97912976238eec508
 }
