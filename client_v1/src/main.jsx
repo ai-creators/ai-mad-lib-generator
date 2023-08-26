2a64f3c8a16df6144cfd17b7e23625dbc75397da
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-08-26 00:44:29 -0400

    Adding auth

--- client_v1/src/main.jsx
+++ client_v1/src/main.jsx
@@ -5,12 +5,15 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./stores/store.js";
 import "./index.css";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <Auth0ProviderWithNavigate>
+          <App />
+        </Auth0ProviderWithNavigate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
