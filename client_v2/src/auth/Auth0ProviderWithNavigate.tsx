c0526cdd546c608bd5183f027528fcfadf4bba10
Anthony McLamb <adylanmclamb@yahoo.com>
2023-12-14 01:50:32 -0500

    Adding the audience

--- client_v2/src/auth/Auth0ProviderWithNavigate.tsx
+++ client_v2/src/auth/Auth0ProviderWithNavigate.tsx
@@ -12,6 +12,11 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain: string = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+  if (!domain || !clientId || !redirectUri || !audience) {
+    throw new Error("INVALID AUTH0 CREDENTIALS");
+  }
 
   const onRedirectCallback = (appState?: AppState) => {
     navigate(appState?.returnTo || window.location.pathname);
@@ -27,6 +32,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience: audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
