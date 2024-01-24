e49fccff18157c004b0fba9a3bef1e945e9e3794
Anthony McLamb <adylanmclamb@yahoo.com>
2024-01-24 13:55:13 -0500

    UPdate navbar

--- client/src/layout/navbar/Navbar.tsx
+++ client/src/layout/navbar/Navbar.tsx
@@ -1,35 +1,39 @@
 import { Link } from "react-router-dom";
 import Container from "../container/Container";
 import ThemeToggle from "@/components/button/ThemeToggle";
+import NavbarMobile from "./navbar-mobile/NavbarMobile";
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 w-full border-b shadow-sm dark:border-border/40 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <Container className="p-2 flex justify-between items-center relative">
         <ul className="flex items-center gap-1 sm:gap-2 md:gap-3">
+          <li className="md:hidden">
+            <NavbarMobile />
+          </li>
           <li>
             <Link to="/">
-              <h1 className="text-lg font-semibold">Ai Adlibs</h1>
+              <h1 className="text-lg font-semibold py-2">Ai Adlibs</h1>
             </Link>
           </li>
         </ul>
-        <ul className="flex items-center gap-5">
-          <li>
+        <ul className="items-center flex gap-5">
+          <li className="hidden md:block">
             <Link to="/" className="py-2 px-2 block">
               Home
             </Link>
           </li>
-          <li>
+          <li className="hidden md:block">
             <Link to="/browse" className="py-2 px-2 block">
               Browse
             </Link>
           </li>
-          <li>
+          <li className="hidden md:block">
             <Link to="/saves" className="py-2 px-2 block">
               Saves
             </Link>
           </li>
-          <li>
+          <li className="hidden md:block">
             <Link to="/" className="py-2 px-2 block">
               Settings
             </Link>
