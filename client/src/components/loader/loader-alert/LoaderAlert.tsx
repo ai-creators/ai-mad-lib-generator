9ff942a6740a4791f572836614596ae083a5ad6b
Anthony McLamb <adylanmclamb@yahoo.com>
2024-01-25 15:03:48 -0500

    Update loader alert

--- client/src/components/loader/loader-alert/LoaderAlert.tsx
+++ client/src/components/loader/loader-alert/LoaderAlert.tsx
@@ -3,11 +3,14 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 type Props = {
   title: string;
   description?: string;
+  className?: string;
 };
 
-const LoaderAlert = ({ title, description }: Props) => {
+const LoaderAlert = ({ title, description, className = "" }: Props) => {
   return (
-    <Alert className="fixed top-10 left-1/2 -translate-x-1/2">
+    <Alert
+      className={`fixed z-[100] top-10 left-1/2 -translate-x-1/2 max-w-fit ${className}`}
+    >
       <AlertTitle>{title}</AlertTitle>
       <AlertDescription>{description}</AlertDescription>
     </Alert>
