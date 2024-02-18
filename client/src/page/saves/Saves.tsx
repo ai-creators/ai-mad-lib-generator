8015c17a1f2d2f66ff3dccbd30e1c49099fec6bb
admclamb <adylanmclamb@yahoo.com>
2024-02-18 08:49:42 -0500

    Update settings an saves

--- client/src/page/saves/Saves.tsx
+++ client/src/page/saves/Saves.tsx
@@ -1,23 +1,25 @@
+import { Card } from "@/components/ui/card";
 import SavesPrompts from "@/features/saves/saves-prompts/SavesPrompts";
 import SavesResponses from "@/features/saves/saves-responses/SavesResponses";
 import Layout from "@/layout/Layout";
 import Container from "@/layout/container/Container";
+import NavbarSidebar from "@/layout/navbar/navbar-sidebar/NavbarSidebar";
 
 const Saves = () => {
   return (
     <Layout>
-      <div data-testid="home">
-        <Container
-          className="px-3 lg:px-0 py-5 grid-cols-9 grid gap-5"
-          width="max-w-3xl"
-        >
-          <section className="col-span-9 lg:col-span-9 flex flex-col gap-5">
-            <SavesPrompts />
-            <SavesResponses />
-          </section>
-          {/* <aside className="col-span-3 "></aside> */}
-        </Container>
-      </div>
+      <Container className="px-3 lg:px-0 py-5 grid-cols-12 grid gap-5">
+        <aside className="hidden lg:flex flex-col gap-5 col-span-3">
+          <Card className="p-2">
+            <NavbarSidebar />
+          </Card>
+        </aside>
+        <section className="col-span-9 lg:col-span-9 flex flex-col gap-5">
+          <SavesPrompts />
+          <SavesResponses />
+        </section>
+        {/* <aside className="col-span-3 "></aside> */}
+      </Container>
     </Layout>
   );
 };
