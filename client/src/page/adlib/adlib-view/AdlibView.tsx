ad2b43e3b4be7c4a0ded05cc2d71804a2fade160
Anthony McLamb <adylanmclamb@yahoo.com>
2024-02-22 13:29:05 -0500

    Adlib adlibview stuff

--- client/src/page/adlib/adlib-view/AdlibView.tsx
+++ client/src/page/adlib/adlib-view/AdlibView.tsx
@@ -5,10 +5,20 @@ import Container from "@/layout/container/Container";
 import AdlibViewer from "@/features/adlib/adlib-viewer/AdlibViewer";
 import ErrorAlert from "@/errors/ErrorAlert";
 import { Button } from "@/components/ui/button";
+import { ClipboardCopy } from "lucide-react";
 
 const AdlibView = () => {
-  const { response, isLoading, error, isSaved, toggleSaveResponse } =
-    useAdlibView();
+  const {
+    response,
+    isLoading,
+    error,
+    isSaved,
+    toggleSaveResponse,
+    copyAdlibLink,
+    isAdlibCopied,
+    isAdlibResponseCopied,
+    copyAdlibResponseLink,
+  } = useAdlibView();
 
   if (isLoading) {
     return <PageLoader />;
@@ -23,10 +33,22 @@ const AdlibView = () => {
         <section className="col-span-9 lg:col-span-9 flex flex-col gap-5">
           <ErrorAlert error={error} />
           {response ? <AdlibViewer response={response} /> : null}
-          <ul>
+          <ul className="flex gap-3">
             <li>
               <Button onClick={toggleSaveResponse} className="w-32">
                 {isSaved ? "Saved Response" : "Save Response"}
+              </Button>
+            </li>
+            <li>
+              <Button onClick={copyAdlibLink} className="w-40">
+                {isAdlibCopied ? "Copied" : "Copy"} Adlib{" "}
+                <ClipboardCopy className="ml-3" size={18} />
+              </Button>
+            </li>
+            <li>
+              <Button onClick={copyAdlibResponseLink} className="w-52">
+                {isAdlibResponseCopied ? "Copied" : "Copy"} Adlib Resposne{" "}
+                <ClipboardCopy className="ml-3" size={18} />
               </Button>
             </li>
           </ul>
