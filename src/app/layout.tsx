7a0cf0a32a4215730095b7c7966e50299fab296a
zhizheng33782@gmail.com <zhizheng33782@gmail.com>
2025-06-19 22:18:21 -0400

    update layout.tsx to use new app router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
             <Providers>{children}</Providers>
           </TRPCReactProvider>
         </body>
-        {/* <CookieConsent /> */}
         <GoogleAnalytics gaId={env.GOOGLE_ANALYTICS_ID ?? ""} />
         <SpeedInsights />
       </html>
